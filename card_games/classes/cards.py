17caee99bbfccca8daee42a3dd999dbdc72f57e9
Ben Dosch <benjamindosch@gmail.com>
2022-02-18 23:13:14 -0600

    changed test file name, cleaned up PlayingCard, added documentation and validation.

--- card_games/classes/cards.py
+++ card_games/classes/cards.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""Modules containing various classes of cards.
-"""
-# https://altcodeunicode.com/alt-codes-playing-card-symbols/
+"""Modules containing various classes of cards."""
 
 
 class Card():
@@ -16,11 +14,30 @@ class PlayingCard(Card):
     deck."""
 
     def __init__(self, value: str, suit: str) -> None:
+        """Initialize an object representing a playing card basied on a given
+        value and suit.
+
+        Args:
+            value (str): Value of the card, A, 1-10, J, Q, or K.
+            suit (str): The suit of the card, Clubs, Dimonds, Hearts or Spade,
+                represented byt the symbols ♣, ♢, ♡, or ♠ respectively.
+        """
         super().__init__()
+        if not isinstance(value, str):
+            raise TypeError("value must be a string")
+        if value not in ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                         "J", "Q", "K"]:
+            raise ValueError("value must be A, 2, 3, 4, 5, 6, 7, 8, 9, 10," + 
+                             " J, Q, or K")
+        if not isinstance(suit, str):
+            raise TypeError("suit must be a string.")
+        if suit not in ["♣", "♢", "♡", "♠"]:
+            raise ValueError("suit must be ♣, ♢, ♡, or ♠")
         self.value = value
         self.suit = suit
 
     def __str__(self):
+        """Changes the string representation of the"""
         return self.name
 
     def __lt__(self, other):
