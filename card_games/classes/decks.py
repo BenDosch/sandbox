305af76948fe93fde318f19af9be3f787bd90a60
Ben Dosch <benjamindosch@gmail.com>
2022-02-18 23:36:46 -0600

    Cleaned up all deck classes and added validation.

--- card_games/classes/decks.py
+++ card_games/classes/decks.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-"""Modules containing various classes of decks.
-"""
-# https://altcodeunicode.com/alt-codes-playing-card-symbols/
+"""Modules containing various classes of decks."""
 
 import random
-from tkinter import N
-PlayingCard = __import__('cards').PlayingCard
+from card_games.classes.cards import Card, PlayingCard
 
 
 class Deck():
@@ -32,33 +29,32 @@ class Deck():
 
         return self.cards.pop(position)
 
-    def place(self, card, position: int=0) -> None:
+    def place(self, card: Card, position: int=0) -> None:
         """Adds a card to the the deck at the specified position.
 
         Args:
             card (Card): A card to place on top of the deck.
             position (int): Location in the deck to draw from. Defaults to 0,
                 the top.
-
-        Returns: 
-            1 on Sucess and None on failure.
         """
+        if not isinstance(card, Card):
+            raise TypeError("card must be a type of Card object.")
+        if not isinstance(position, int):
+            raise TypeError("position must be an int.")
+
         if position >= len(self.cards) or position < (-1 * len(self.cards)):
-            print("There are not that many cards in the deck.")
-            return None
+            raise Exception("There are not that many cards in the deck.")
 
         self.cards.insert(position, card)
-        return 1
 
     def shuffel(self):
         """Shuffles the deck, rearangeig the cards in a random order."""
         random.shuffle(self.cards)
 
-
     def __str__(self):
-        """Reviels the cards in the deck."""
+        """Sets the string representation of the deck to the cards inside."""
         string = ""
-        
+
         for card in self.cards:
             if string != "":
                 string += ", " + card.name
@@ -73,14 +69,18 @@ class PlayingCardDeck(Deck):
 
     def __init__(self, cards: list=None) -> None:
         super().__init__()
+        if not isinstance(cards, list):
+            raise TypeError("cards must be a list")
+        if not all([isinstance(x, Card) for x in cards]):
+            raise TypeError("All objects in cards must be Card objects.")
+
         if cards:
             self.cards = cards
         else:
             self.reset()
 
     def reset(self) -> None:
-        """Returns all cards to the deck and shuffels it.
-        """
+        """Returns all cards to the deck and shuffels it."""
         self.cards.clear()
 
         suits = ["♣", "♢", "♡", "♠"]
@@ -91,7 +91,7 @@ class PlayingCardDeck(Deck):
                 self.cards.append(PlayingCard(value, suit))
 
         self.shuffel()
-    
+
     def show(self):
         """Shows the playing cards in deck."""
         print(type(self.cards))
@@ -99,13 +99,13 @@ class PlayingCardDeck(Deck):
 
 
 
-class Hand(PlayingCardDeck):
-    """A hand of cards"""
+class PlayingCardHand(PlayingCardDeck):
+    """A hand of playing cards"""
 
     def __init__(self) -> None:
         super().__init__()
 
-    def card_from_deck(self, deck, position: int=0):
+    def draw(self, deck: Deck, position: int=0):
         """Draws a card from the specified deck.
 
         Args:
@@ -113,16 +113,28 @@ class Hand(PlayingCardDeck):
             position (int, optional): Where in the deck to draw from. Defaults
                 to the top.
         """
+        if not isinstance(deck, Deck):
+            raise TypeError("deck must be a Deck object")
+        if not isinstance(position, int):
+            raise TypeError("position must be an int.")
+
         self.cards.append(deck.draw(position))
 
-    def card_to_deck(self, deck, card, position: int=0):
+    def place(self, deck: Deck, card: Card, position: int=0):
         """Places a card from the hand into a deck."""
+        if not isinstance(deck, Deck):
+            raise TypeError("deck must be a Deck object.")
+        if not isinstance(card, Card):
+            raise TypeError("card must be a Card object.")
+        if not isinstance(position, int):
+            raise TypeError("position must be an int.")
+
         if card in self.cards:
             self.cards.remove(card)
             deck.place(card, position)
         else:
             print("Card was not in hand.")
-    
+
     def sort_number(self):
         """Sorts hand by number."""
         self.cards.sort()
@@ -137,45 +149,8 @@ class Hand(PlayingCardDeck):
         self.cards = temp
 
     def reset(self):
-        """Returns the hand to the default starting point. Empty"""
+        """Returns the hand to the default starting point of empty"""
         self.cards = []
 
 if __name__ == "__main__":
-    deck = PlayingCardDeck()
-    deck.show()
-    print("Checkpoint 1 -------")
-    print(deck.draw(0))
-    print(deck.draw(-1))
-    print("Checkpoint 2 -------")
-    deck.show()
-    deck.shuffel()
-    print("Checkpoint 3 -------")
-    deck.show()
-    deck.place(card=PlayingCard("top", "test"), position=0)
-    deck.place(card=PlayingCard("bottom", "test"), position=-1)
-    deck.place(card=PlayingCard("to high", "test"), position=60)
-    deck.place(card=PlayingCard("to low", "test"), position=-60)
-    print("Checkpoint 4 -------")
-    deck.show()
-    print("======")
-    deck.reset()
-    deck.show()
-    hand = Hand()
-    hand.show()
-    for i in range(5):
-        hand.card_from_deck(deck)
-    hand.show()
-    deck.show()
-    print("Checkpoint 5 -------")
-    hand.sort_number()
-    hand.show()
-    hand.sort_suit()
-    hand.show()
-    print("Checkpoint 6 -------")
-    hand.card_to_deck(deck, hand.cards[0])
-    hand.show()
-    deck.show()
-    print("Checkpoint 7 -------")
-    suits = ["♣", "♢", "♡", "♠"]
-    values = ["A", "2", "3", "4", "5", "6", "7",
-              "8", "9", "10", "J", "Q", "K"]
+    pass
